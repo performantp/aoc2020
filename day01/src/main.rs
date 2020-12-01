9e578680df89bdb6da6a4cda0f63cd1954fc82ec
performantp <performantp@performant.ch>
2020-12-01 19:28:55 +0100

    day01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,60 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::{File, read};
+use std::io;
+use std::io::{BufReader, BufRead, Read};
+
+fn main() -> io::Result<()>{
+
+    let file = File::open("src/day01.txt")?;
+    let mut reader = BufReader::new(file);
+    let lines = reader.by_ref().lines();
+    let input_vec:Vec<i32> = lines.map(|i| i.ok().unwrap().parse().unwrap()).collect();
+
+    println!("solution part1: {}", find_sum_and_multiply(&input_vec, 2020));
+    println!("solution part2: {}", find_sum_and_multiply_several(&input_vec, 2020));
+    println!("execution finished");
+
+    Ok(())
+}
+
+fn find_sum_and_multiply(input:&Vec<i32>, sum_to_find:i32) -> i32{
+    //let's try a naive solution first
+    for i in input {
+        let candidate1:i32 = *i;
+        for j in input {
+            let candidate2:i32 = *j;
+            //println!("candidate2:{}",candidate2);
+            println!("candidate1:{} candidate2:{},added:{}",candidate1,candidate2,candidate1+candidate2);
+            if candidate1+candidate2 == sum_to_find {
+                println!("solution part 1: {}",candidate1*candidate2);
+                return candidate1*candidate2;
+            }
+        }
+    }
+    return -1;
+}
+fn find_sum_and_multiply_several(input:&Vec<i32>, sum_to_find:i32) -> i32{
+    for i in input {
+        let candidate1:i32 = *i;
+        for j in input {
+            let candidate2:i32 = *j;
+            for k in input {
+                let candidate3: i32 = *k;
+                if candidate1 + candidate2 + candidate3 == sum_to_find {
+                    println!("solution part 2: {}", candidate1 * candidate2 * candidate3);
+                    return candidate1 * candidate2 * candidate3;
+                }
+            }
+        }
+    }
+    return -1;
+}
+#[cfg(test)]
+mod tests{
+
+    #[test]
+    fn test_from_example(){
+
+        let solution = 514579;
+    }
+
 }
